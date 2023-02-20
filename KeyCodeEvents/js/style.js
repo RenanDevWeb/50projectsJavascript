a3fdba0dee1ed04bb322a67d3e621afd0f485e80
Renan <renansalmazio@gmail.com>
2023-02-20 12:11:16 -0300

    projeto keycode

---
+++ KeyCodeEvents/js/style.js
@@ -0,0 +1,22 @@
+const insert = document.getElementById('insert')
+const template =
+
+
+window.addEventListener("keydown",(event) => {
+    console.log(event)
+    insert.innerHTML =   `<div class="key">
+    ${event.key === " " ? 'Space' : event.key}
+    <small>event.key</small>
+    </div>
+    
+    <div class="key">
+    ${event.keyCode}
+    <small>event.keyCode</small>
+    </div>
+    
+    <div class="key">
+    ${event.code}
+    <small>event.code</small>
+    </div>
+    `
+})
